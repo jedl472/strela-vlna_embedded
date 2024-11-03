3c8c3e777d6ac839c448e67b36674b69416b22cb
jedl472 <jodltomas6@gmail.com>
2024-11-03 16:11:28 +0100

    Update strela-vlna_embedded.ino

--- strela-vlna_embedded.ino
+++ strela-vlna_embedded.ino
@@ -171,14 +171,14 @@ void loop() {
             case 0:
               display_u8g2.drawStr(0, 10, "Nastavte typ akce: ");
               display_u8g2.drawStr(0, 33, "_");
-              display_u8g2.drawStr(0, 30, String(volbyUzivatele[1]).c_str());
-              display_u8g2.drawStr(20, 30, String(volbyUzivatele[2]).c_str());
+              display_u8g2.drawStr(0, 30, String(volbyUzivatele[0]).c_str());
+              display_u8g2.drawStr(20, 30, String(volbyUzivatele[1]).c_str());
               break;
             case 1:
               display_u8g2.drawStr(0, 10, "Nastavte typ ulohy: ");
               display_u8g2.drawStr(20, 33, "_");
-              display_u8g2.drawStr(0, 30, String(volbyUzivatele[1]).c_str());
-              display_u8g2.drawStr(20, 30, String(volbyUzivatele[2]).c_str());
+              display_u8g2.drawStr(0, 30, String(volbyUzivatele[0]).c_str());
+              display_u8g2.drawStr(20, 30, String(volbyUzivatele[1]).c_str());
               break;
             default:
               break;
